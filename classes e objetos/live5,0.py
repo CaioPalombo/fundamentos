ed75becc0aa76400d2996a2a7ac608e517dcea79
CaioPalombo <caio.tomaz-palombo@unesp.br>
2025-07-24 11:37:27 -0300

    exemplo live 5

--- classes e objetos/live5,0.py
+++ classes e objetos/live5,0.py
@@ -1,10 +1,16 @@
 '''Implementar um container'''
+from collections.abc import Container, Sized, Collection
 
-class Container:
+class Caixa(Collection):
     def __init__(self, seq):
         self.seq = seq
-        ...
-    def __contains__(self, other):
-        return None
-        ...
     
+    def __contains__(self, other):
+        return other in list(self.seq)
+        
+    def __len__(self):
+        return len(self.seq)
+    
+    def __inter__(self):
+        return self
+
