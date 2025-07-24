89f45aba2695ce21ec515a9ba4cd3bd8a0c603f5
CaioPalombo <caio.tomaz-palombo@unesp.br>
2025-07-24 10:39:10 -0300

    Live 5 24/07

---
+++ classes e objetos/live5,0.py
@@ -0,0 +1,10 @@
+'''Implementar um container'''
+
+class Container:
+    def __init__(self, seq):
+        self.seq = seq
+        ...
+    def __contains__(self, other):
+        return None
+        ...
+    
